371d7d439768288718f20205a3d847489ff850ae
Ernest0203 <ernest2332@gmail.com>
2020-11-18 09:58:04 +0200

    Updates

---
+++ precache-manifest.a503a4825a684a7904f70acd111cd4b9.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9259bad68796a40692a225ddbde3334c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ebfd5825c9c5f8aa3a74",
+    "url": "./static/css/main.61aed7d5.chunk.css"
+  },
+  {
+    "revision": "2b271cbf6485f7752713",
+    "url": "./static/js/2.095b7395.chunk.js"
+  },
+  {
+    "revision": "23ec6c7f0411a6141095a6c739b32980",
+    "url": "./static/js/2.095b7395.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ebfd5825c9c5f8aa3a74",
+    "url": "./static/js/main.dfa005eb.chunk.js"
+  },
+  {
+    "revision": "d90c5b93796701d5570a",
+    "url": "./static/js/runtime-main.8b0ae790.js"
+  },
+  {
+    "revision": "e2cf3dc2f079bf3d5185a02552f153c4",
+    "url": "./static/media/ProximaNova-Bold.e2cf3dc2.woff"
+  },
+  {
+    "revision": "eb74a591665045d87eabfb2dc759be0a",
+    "url": "./static/media/ProximaNova-Light.eb74a591.woff"
+  },
+  {
+    "revision": "2d2ae2556b24a45ff8d5ed86b07b5783",
+    "url": "./static/media/ProximaNova-Regular.2d2ae255.woff"
+  },
+  {
+    "revision": "8feb512e78c18175c552af2be6ae2e02",
+    "url": "./static/media/ProximaNova-Semibold.8feb512e.woff"
+  },
+  {
+    "revision": "68b4e79928ae22dc774bb9a25abdac15",
+    "url": "./static/media/arrow.68b4e799.svg"
+  },
+  {
+    "revision": "6e56002e3c3e16d47b00bf850e5d9e12",
+    "url": "./static/media/avatar-big.6e56002e.png"
+  },
+  {
+    "revision": "7e0eba1fe0bb488771640da72aefa68c",
+    "url": "./static/media/down.7e0eba1f.svg"
+  },
+  {
+    "revision": "77af777b5a181a9c99234b4b88ad6c39",
+    "url": "./static/media/grid.77af777b.svg"
+  },
+  {
+    "revision": "cf45888c9dc599b6ca7100b3c252a61c",
+    "url": "./static/media/pen.cf45888c.svg"
+  },
+  {
+    "revision": "61286944e98d7f79f579600289f7dac1",
+    "url": "./static/media/search.61286944.svg"
+  },
+  {
+    "revision": "50a9a1fa2a1a0b94efbdd3fd47e9317b",
+    "url": "./static/media/settings.50a9a1fa.svg"
+  },
+  {
+    "revision": "3c5fd0c30adce89734eac7123f6ba30f",
+    "url": "./static/media/sort.3c5fd0c3.svg"
+  },
+  {
+    "revision": "306a0c19e15d77ec8fced55eadd91bc4",
+    "url": "./static/media/up.306a0c19.svg"
+  }
+]);
